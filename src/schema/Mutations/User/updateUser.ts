693f8fa8492f58fd1ecd30eac0f6d400e99a4876
thomassalduende <thomi44sal@gmail.com>
2022-11-14 20:44:44 -0300

    upd act

--- src/schema/Mutations/User/updateUser.ts
+++ src/schema/Mutations/User/updateUser.ts
@@ -1,6 +1,6 @@
 import {GraphQLString, GraphQLID, GraphQLNonNull, GraphQLInt, GraphQLBoolean, GraphQLInputObjectType} from 'graphql';
 import { Users } from '../../../Entities/Users';
-import { comparePassword } from '../../../libs/bcrypt';
+import { comparePassword } from '../../../libs/compare';
 import {MessageType} from '../../TypeDefs/Message'
 
 
@@ -8,40 +8,34 @@ export const UpdateUser = {
     type: MessageType,
     args: {
         dni: {type: new GraphQLNonNull(GraphQLID)},
-        input: new GraphQLInputObjectType({
-            name: 'UserInput',
-            fields: () =>({
-                nombre: {type: new GraphQLNonNull(GraphQLString)},
-                apellido: {type: new GraphQLNonNull(GraphQLString)},
-                email: {type: new GraphQLNonNull(GraphQLString)},
-                telefono: {type: new GraphQLNonNull(GraphQLInt)},
-                direccion: {type: new GraphQLNonNull(GraphQLString)},
-                old_password: {type: new GraphQLNonNull(GraphQLString)},
-                new_password:{type: new GraphQLNonNull(GraphQLString)},
-                es_admin: {type: new GraphQLNonNull(GraphQLBoolean)},
-            }),
-        }),
+        nombre: {type: new GraphQLNonNull(GraphQLString)},
+        apellido: {type: new GraphQLNonNull(GraphQLString)},
+        email: {type: new GraphQLNonNull(GraphQLString)},
+        telefono: {type: new GraphQLNonNull(GraphQLInt)},
+        direccion: {type: new GraphQLNonNull(GraphQLString)},
+        password: {type: new GraphQLNonNull(GraphQLString)},
+        es_admin: {type: new GraphQLNonNull(GraphQLBoolean)},  
     },
-    async resolve(_:any, {dni, input}: any) {
+    async resolve(_:any, args: any) {
         // const result = await updateUser(args.dni, args.nombre, args.apellido, args.email, args.telefono, args.direccion, args.password, args.es_admin);
-        const userFound = await Users.findOneBy({dni});
-        if (!userFound) throw new Error('User not found');
+        const userFound = await Users.findOneBy(args.dni);
+        if (!userFound)
+            return {
+                success:false,
+                message: "User not found"
+            }
 
         // Compara la contraseña vieja con la nueva
-        const isMatch = await comparePassword(
-            userFound?.password as string,
-            input.old_password
-        );
-        if (!isMatch) throw new Error('Passwords does not match')
+        const isMatch = await comparePassword(args.password, userFound.password)
 
-        // Actualiza la contraseña
-        const new_password = input.new_password;
-        delete input.old_password;
-        delete input.new_password;
+        if(!isMatch) return{
+                success: false,
+                message: "Password is incorrect"
+        }
+        
+     
 
-        input.password = new_password;
-
-        const response = await Users.update({dni}, input);
+        const response = await Users.update((args.dni), (args.nombre, args.apellido, args.email, args.telefono, args.direccion, args.password, args.es_admin));
 
         if (response.affected === 0) return {message: "User not found"};
 
