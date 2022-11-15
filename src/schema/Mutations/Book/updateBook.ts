18eee1936e12ae82b3b1b424ac3e13d672516f08
thomassalduende <thomi44sal@gmail.com>
2022-11-14 21:13:04 -0300

    book upd

---
+++ src/schema/Mutations/Book/updateBook.ts
@@ -0,0 +1,41 @@
+import {GraphQLString, GraphQLID, GraphQLNonNull, GraphQLFloat, GraphQLInt} from 'graphql';
+import { Books } from '../../../Entities/Books';
+import { MessageType } from '../../TypeDefs/Message';
+
+
+
+export const UpdateBook = {
+    type: MessageType,
+    args: {
+        isbn: {type: new GraphQLNonNull(GraphQLID)},
+        nombre: {type: new GraphQLNonNull(GraphQLString)},
+        precio: {type: new GraphQLNonNull(GraphQLInt)},
+        stock: {type: new GraphQLNonNull(GraphQLInt)},
+        stock_min: {type: new GraphQLNonNull(GraphQLInt)},
+        id_valoracion: {type: new GraphQLNonNull (GraphQLInt)},
+        id_genero: {type: new GraphQLNonNull (GraphQLString)},
+        id_oferta: {type: new GraphQLNonNull (GraphQLFloat)},
+        dni_autor: {type: new GraphQLNonNull (GraphQLInt)},
+        id_editorial: {type: new GraphQLNonNull (GraphQLInt)},
+        id_linea_carrito: {type: new GraphQLNonNull (GraphQLInt)}
+    },
+    async resolve(_:any, args: any) {
+
+        const bookFound = await Books.findOneBy(args.isbn)
+
+        if (!bookFound) return {
+            success: false,
+            message: "Book not found"
+        }
+
+        const response = await Books.update((args.isbn), (args.nombre, args.precio, args.stock, args.stock_min, args.id_valoracion, args.id_genero, args.id_oferta, args.dni_autor, args.id_editorial, args.id_linea_carrito))
+
+        if(response.affected === 0) return{message: "Book not found"}; 
+        
+        return {
+            success: true, 
+            message: "Update Book successfully"
+        }
+        
+    }
+}
