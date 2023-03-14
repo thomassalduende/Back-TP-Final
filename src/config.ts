ac9ae18170470bb2da09685d4c1ad791303389d8
thomassalduende <thomi44sal@gmail.com>
2023-03-14 09:32:42 -0300

    prueba deploy

--- src/config.ts
+++ src/config.ts
@@ -1,13 +1,13 @@
 import { config } from "dotenv";
 config()
 
-export const PORT = process.env.PORT || '3000'
+export const PORT = process.env.PORT || 3000
 
 export const DB_USERNAME = process.env.DB_USERNAME || 'postgres';
 export const DB_PASSWORD = process.env.DB_PASSWORD || 'Thomas44';
 export const DB_DATABASE= process.env.DB_DATABASE || 'TP-Final';
 export const DB_HOST = process.env.DB_HOST || 'localhost';
-export const DB_PORT = process.env.DB_PORT || '5432';
+export const DB_PORT = process.env.DB_PORT || 5432;
 
 
 export const ACCESS_TOKEN_MP = process.env.ACCESS_TOKEN_MP
