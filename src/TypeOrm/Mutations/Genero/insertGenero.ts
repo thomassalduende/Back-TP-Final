e75a5df90ec11c6a75ae6e132caf57fd074e5e0e
thomassalduende <thomi44sal@gmail.com>
2023-04-14 17:27:26 -0300

    insert genero arreglado

--- src/TypeOrm/Mutations/Genero/insertGenero.ts
+++ src/TypeOrm/Mutations/Genero/insertGenero.ts
@@ -4,16 +4,18 @@ import { getElementByNombre } from "../Utilities/getElementByNombre";
 
 export async function insertGenero(nombreGenero: string, url_imagen: string) : Promise<Genero>{
 
-    const existeGenero = existsNombre(nombreGenero, Genero)
-    const genero = new Genero();
+    const existeGenero = await existsNombre(nombreGenero, Genero)
+    console.log(existeGenero)
 
-    if(!existeGenero){
-        
-        genero.nombre = nombreGenero;
-        genero.url_imagen = url_imagen;
-
-        await genero.save()
+    if(existeGenero){
+        throw `ERROR, EL GENERO ${nombreGenero} YA EXISTE`
     }
+    const genero = new Genero();
+        
+    genero.nombre = nombreGenero;
+    genero.url_imagen = url_imagen;
+
+    await genero.save()
 
     return getElementByNombre(nombreGenero, Genero)
 
--- src/TypeOrm/Mutations/Utilities/Exists.ts
+++ src/TypeOrm/Mutations/Utilities/Exists.ts
@@ -8,5 +8,5 @@ export async function existsNombre(nombre: any, entitie: any) {
             nombre: ILike(nombre)
         }
     })
-    return resultado[0]? true : false
+    return resultado[0] ? true : false
 }
