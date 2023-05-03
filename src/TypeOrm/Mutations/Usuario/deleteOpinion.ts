4a8a762d1169e912933a88522b10f0c899976cd1
thomassalduende <thomi44sal@gmail.com>
2023-05-03 11:14:33 -0300

    delete comentario arreglado

--- src/TypeOrm/Mutations/Usuario/deleteOpinion.ts
+++ src/TypeOrm/Mutations/Usuario/deleteOpinion.ts
@@ -19,8 +19,12 @@ export async function deleteOpinion(id: number, isbn: string) {
     if(user[0].opinion){
 
         const pos = user[0].opinion.findIndex(obj => obj.book.isbn === isbn);
-        await user[0].opinion[pos].remove();
-        user[0].opinion.splice(pos, 1);
+        if(pos !== -1){
+            await user[0].opinion[pos].remove();
+            user[0].opinion.splice(pos, 1);
+            await user[0].save()
+        }else{
+            throw new Error('No existe el comentario')
+        }
     }
-    
 }
